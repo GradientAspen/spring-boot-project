28cc046a36c215c520df1f829a36da4eb350f4a1
testSouth <southparaplan@gmail.com>
2023-12-21 18:23:41 +0200

    added EntityNotFoundException class

--- src/main/java/com/project/springbootproject/controller/BookController.java
+++ src/main/java/com/project/springbootproject/controller/BookController.java
@@ -1,4 +1,29 @@
 package com.project.springbootproject.controller;
 
+import com.project.springbootproject.model.Book;
+import com.project.springbootproject.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/books")
 public class BookController {
+    private final BookService bookService;
+
+    @GetMapping
+    public List<Book> findALL() {
+        return bookService.findAll();
+    }
+
+    @PostMapping
+    public Book save(@RequestBody Book book) {
+        return bookService.save(book);
+    }
 }
