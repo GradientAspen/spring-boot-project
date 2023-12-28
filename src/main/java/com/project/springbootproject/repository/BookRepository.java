4b8ea17a0fb85025bc164127c728e69f407ce4c0
testSouth <southparaplan@gmail.com>
2023-12-28 15:45:47 +0200

    added JpaRepository and fixed BookRepository interface

--- src/main/java/com/project/springbootproject/repository/BookRepository.java
+++ src/main/java/com/project/springbootproject/repository/BookRepository.java
@@ -1,13 +1,8 @@
 package com.project.springbootproject.repository;
 
 import com.project.springbootproject.model.Book;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository {
-    Book save(Book book);
+public interface BookRepository extends JpaRepository<Book,Long> {
 
-    List<Book> findAll();
-
-    Optional<Book> findById(Long id);
 }
