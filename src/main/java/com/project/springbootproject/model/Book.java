0c923b7622475c62f3a94fcbfb36448efe16d5c9
testSouth <southparaplan@gmail.com>
2024-01-06 19:26:11 +0200

    change field isbn

--- src/main/java/com/project/springbootproject/model/Book.java
+++ src/main/java/com/project/springbootproject/model/Book.java
@@ -6,11 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.math.BigDecimal;
 import lombok.Data;
-import lombok.NonNull;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
+
+import java.math.BigDecimal;
 
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
@@ -21,14 +21,13 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NonNull
+
     private String title;
-    @NonNull
+
     private String author;
-    @NonNull
-    @Column(unique = true)
+
     private String isbn;
-    @NonNull
+
     private BigDecimal price;
     private String description;
     private String coverImage;
