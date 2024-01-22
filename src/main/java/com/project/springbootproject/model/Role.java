afbc40801a1e975a3326cd1601a9542e90399b36
testSouth <southparaplan@gmail.com>
2024-01-23 00:07:27 +0200

    added changeSet for class Role

--- src/main/java/com/project/springbootproject/model/Role.java
+++ src/main/java/com/project/springbootproject/model/Role.java
@@ -1,13 +1,17 @@
 package com.project.springbootproject.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.springframework.security.core.GrantedAuthority;
 
+@Entity
+@Table(name = "roles")
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
