5791ffa8da557a263b8156245a32ae72c9b425d8
testSouth <southparaplan@gmail.com>
2024-01-24 19:25:29 +0200

    add line antMatcher(/swagger-ui/**) to the SecurityFilterChain bean

--- src/main/java/com/project/springbootproject/model/User.java
+++ src/main/java/com/project/springbootproject/model/User.java
@@ -2,6 +2,7 @@ package com.project.springbootproject.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -41,7 +42,8 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private boolean isDeleted = false;
     private byte[] salt;
-    @ManyToMany
+
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
