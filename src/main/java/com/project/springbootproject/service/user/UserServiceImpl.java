94adc8ed01e27b5543f113d8eda555b5cbe4654e
testSouth <southparaplan@gmail.com>
2024-01-25 22:01:11 +0200

    added role User for all new registrations

--- src/main/java/com/project/springbootproject/service/user/UserServiceImpl.java
+++ src/main/java/com/project/springbootproject/service/user/UserServiceImpl.java
@@ -4,8 +4,11 @@ import com.project.springbootproject.dto.userdto.UserDto;
 import com.project.springbootproject.dto.userdto.UserRequestDto;
 import com.project.springbootproject.exception.RegistrationException;
 import com.project.springbootproject.mapper.UserMapper;
+import com.project.springbootproject.model.Role;
 import com.project.springbootproject.model.User;
+import com.project.springbootproject.repository.role.RoleRepository;
 import com.project.springbootproject.repository.user.UserRepository;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserDto register(UserRequestDto userRequestDto) {
@@ -29,6 +33,9 @@ public class UserServiceImpl implements UserService {
         user.setFirstName(userRequestDto.getFirstName());
         user.setLastName(userRequestDto.getLastName());
         user.setShippingAddress(userRequestDto.getShippingAddress());
+        Role userRole = roleRepository.findByRole(Role.RoleName.USER).orElseThrow(
+                () -> new IllegalStateException("Role User not found"));
+        user.setRoles(Collections.singleton(userRole));
         User saveUser = userRepository.save(user);
         return userMapper.toUserDto(saveUser);
     }
