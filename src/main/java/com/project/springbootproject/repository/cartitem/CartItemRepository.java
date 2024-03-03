e2bf10b4644b72d4ff1b014746113454508a956c
testSouth <southparaplan@gmail.com>
2024-03-03 11:44:31 +0200

    added Dockerfile

--- src/main/java/com/project/springbootproject/repository/cartitem/CartItemRepository.java
+++ src/main/java/com/project/springbootproject/repository/cartitem/CartItemRepository.java
@@ -2,9 +2,11 @@ package com.project.springbootproject.repository.cartitem;
 
 import com.project.springbootproject.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    @Modifying
     @Query("UPDATE CartItem ci SET ci.isDeleted = true WHERE ci.shoppingCart.id = :id")
     void deleteCartItemsFromDb(Long id);
 }
